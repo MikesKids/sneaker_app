214b7da959694ae162e719a9cf4f533809c39ff8
MikesKids <michael.dantzler@gmail.com>
2024-07-21 10:26:59 -0400

    add database logic

---
+++ back-end/db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS sneakers_db;
+CREATE DATABASE sneakers_db;
+
+\c sneakers_db;
+
+CREATE TABLE sneakers (
+    id SERIAL PRIMARY KEY,
+    brand TEXT NOT NULL,
+    model TEXT NOT NULL,
+    price INTEGER,
+    category TEXT,
+    color TEXT, 
+    is_favorite BOOL
+);
