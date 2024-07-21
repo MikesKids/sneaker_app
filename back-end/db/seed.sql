55f6818ca4de696b378edaa6f979b17b65753379
MikesKids <michael.dantzler@gmail.com>
2024-07-21 10:33:54 -0400

    Create seed.sql

---
+++ back-end/db/seed.sql
@@ -0,0 +1,8 @@
+\c sneakers_db;
+
+INSERT INTO sneakers
+    (brand, model, price, category, color, is_favorite)
+VALUES
+    ('Nike', 'Air Max 270', 130, 'Athletic', 'white', false),
+    ('Jordan', 'Air Jordan 1', 220, 'Basketball', 'white & Red', true),
+    ('Adidas', 'Gazelle', 90, 'Walking', 'blue & white', false);
