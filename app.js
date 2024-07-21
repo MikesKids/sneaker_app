4bdf7d4072bc437d17a271c781bcb72f3b3bf9cd
MarcusB86 <marcusb08@gmail.com>
2024-07-21 10:39:04 -0400

    set up index route for controllers, set up 404, set up sneakers ROUTE in app.js

--- app.js
+++ app.js
@@ -13,5 +13,12 @@ app.get("/", (req, res) => {
   res.send("Welcome to Sneakers App Sucka");
 });
 
+const sneakersController = require("./back-end/controllers/sneakersController.js");
+app.use("/sneakers", sneakersController);
+
+app.get("*", (req, res) => {
+    res.status(404).send("Page not found, do better");
+})
+
 
 module.exports = app;
